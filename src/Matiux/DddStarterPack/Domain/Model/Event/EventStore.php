8efec684d9a40ed095d6e0a4bbca080e81b47ad8
Iuxmat <m.galacci@gmail.com>
2017-12-03 22:14:08 +0100

    Sistemazione modelli eventi

--- src/Matiux/DddStarterPack/Domain/Model/Event/EventStore.php
+++ src/Matiux/DddStarterPack/Domain/Model/Event/EventStore.php
@@ -2,15 +2,13 @@
 
 namespace DddStarterPack\Domain\Model\Event;
 
-use DddStarterPack\Domain\Event\Subscriber\Serializer;
-
 interface EventStore
 {
     public function add(DomainEvent $storedEvent);
 
     public function addBulk(\ArrayObject $bulkEvents);
 
-    public function allStoredEventsSince($anEventId): \ArrayObject;
+    public function allStoredEventsSince(?int $anEventId): \ArrayObject;
 
     public function nextId(): int;
 
