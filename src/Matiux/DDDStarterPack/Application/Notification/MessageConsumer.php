4765f1ecebc85baec592ec3730e4574f3d26425f
matiux <m.galacci@gmail.com>
2017-12-23 00:50:08 +0100

    aggiunti metodi per rimozione messaggi coda

--- src/Matiux/DDDStarterPack/Application/Notification/MessageConsumer.php
+++ src/Matiux/DDDStarterPack/Application/Notification/MessageConsumer.php
@@ -7,6 +7,10 @@ interface MessageConsumer
     public function open(string $exchangeName);
 
     public function receiveMessage(int $maximumNumberOfMessages = 1);
+    
+    public function deleteMessage($messageId);
+
+    public function deleteMessageBatch(array $messagesId);
 
     public function close($exchangeName);
 }
