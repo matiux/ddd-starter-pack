23576e620fc635ef281b4693f880b512facc6033
Matiux <m.galacci@gmail.com>
2018-03-28 12:28:41 +0200

    Aggiunto UUID path per controllo regex

--- src/Matiux/DDDStarterPack/Domain/Model/BasicEntityId.php
+++ src/Matiux/DDDStarterPack/Domain/Model/BasicEntityId.php
@@ -8,6 +8,8 @@ abstract class BasicEntityId implements EntityId
 {
     private $id;
 
+    const UUID_PATTERN = '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/';
+
     protected function __construct($anId = false)
     {
         $this->verifyInputId($anId);
