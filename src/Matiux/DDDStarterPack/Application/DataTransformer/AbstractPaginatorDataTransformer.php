903438b2c93a2d69c766dcdfa81001a86469e112
Matteo <m.galacci@gmail.com>
2019-03-14 15:52:44 +0100

    Fixnamespace

--- src/Matiux/DDDStarterPack/Application/DataTransformer/AbstractPaginatorDataTransformer.php
+++ src/Matiux/DDDStarterPack/Application/DataTransformer/AbstractPaginatorDataTransformer.php
@@ -2,7 +2,7 @@
 
 namespace DDDStarterPack\Application\DataTransformer;
 
-use DDDStarterPack\Domain\Aggregate\Repository\Paginator\Paginator;
+use DDDStarterPack\Domain\Repository\Paginator\Paginator;
 use LogicException;
 use Traversable;
 
