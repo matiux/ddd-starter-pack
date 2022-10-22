002e6410e7a905c03cd71ed2de1143489e9f7c15
Matteo Galacci <m.galacci@gmail.com>
2022-10-23 01:00:02 +0200

    feat: adds DateTimeRFC doctrine implementation

---
+++ src/Matiux/DDDStarterPack/Type/DoctrineDateTimeRFC.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DDDStarterPack\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\StringType;
+use Exception;
+
+class DoctrineDateTimeRFC extends StringType
+{
+    /**
+     * @psalm-suppress MoreSpecificImplementedParamType
+     *
+     * @param null|DateTimeRFC $value
+     * @param AbstractPlatform $platform
+     *
+     * @return null|string
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): null|string
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return (string) $value;
+    }
+
+    /**
+     * @psalm-suppress MoreSpecificImplementedParamType
+     *
+     * @param null|string      $value
+     * @param AbstractPlatform $platform
+     *
+     * @throws Exception
+     *
+     * @return null|DateTimeRFC
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): null|DateTimeRFC
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        return DateTimeRFC::createFrom($value);
+    }
+}
