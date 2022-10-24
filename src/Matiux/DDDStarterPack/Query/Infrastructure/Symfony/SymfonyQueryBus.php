90ba1bb4b9204545d3a6f7d2fc754be480fd1bde
Matteo Galacci <m.galacci@gmail.com>
2022-10-24 12:15:30 +0200

    fixs Query interfaces's psalm errors

--- src/Matiux/DDDStarterPack/Query/Infrastructure/Symfony/SymfonyQueryBus.php
+++ src/Matiux/DDDStarterPack/Query/Infrastructure/Symfony/SymfonyQueryBus.php
@@ -1,27 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DDDStarterPack\Query\Infrastructure\Symfony;
 
 use DDDStarterPack\Query\Query;
 use DDDStarterPack\Query\QueryBus;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\HandleTrait;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Webmozart\Assert\Assert;
+
 class SymfonyQueryBus implements QueryBus
 {
     use HandleTrait;
 
-     public function __construct(MessageBusInterface $queryBus)
-      {
-          $this->messageBus = $queryBus;
-      }
+    public function __construct(MessageBusInterface $queryBus)
+    {
+        $this->messageBus = $queryBus;
+    }
 
     public function query(Query $query): mixed
     {
         try {
             return $this->handle($query);
-        }  catch (HandlerFailedException $e) {
-            throw first($e->getNestedExceptions());
+        } catch (HandlerFailedException $e) {
+            $first = current($e->getNestedExceptions());
+            Assert::isInstanceOf($first, \Exception::class);
+
+            throw $first;
         }
     }
 }
--- src/Matiux/DDDStarterPack/Query/Query.php
+++ src/Matiux/DDDStarterPack/Query/Query.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DDDStarterPack\Query;
 
 interface Query
 {
-
 }
