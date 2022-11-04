dd54dfca1df5a7a60beab0f7911636ee78fab155
Matteo Galacci <m.galacci@gmail.com>
2022-11-04 23:39:14 +0100

    fix: suppress psalm error

--- src/Matiux/DDDStarterPack/Tool/EnvVarUtil.php
+++ src/Matiux/DDDStarterPack/Tool/EnvVarUtil.php
@@ -6,6 +6,14 @@ namespace DDDStarterPack\Tool;
 
 class EnvVarUtil
 {
+    /**
+     * @psalm-suppress PossiblyInvalidCast
+     *
+     * @param non-empty-string $name
+     * @param string           $default
+     *
+     * @return string
+     */
     public static function get(string $name, string $default = ''): string
     {
         return getenv($name) ?: (string) ($_ENV[$name] ?? $default);
