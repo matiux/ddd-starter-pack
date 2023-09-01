e4a7c8c8fa5786fcd6e84390342109725d6c2269
Matteo Galacci <m.galacci@gmail.com>
2023-09-01 10:15:42 +0200

    feat: adds base class for json WhereEquals filter on mariadb

---
+++ src/Matiux/DDDStarterPack/Repository/Doctrine/Repository/Filter/DoctrineJsonWhereEqualsFilterApplier.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DDDStarterPack\Repository\Doctrine\Repository\Filter;
+
+use DDDStarterPack\Repository\Filter\FilterAppliersRegistry;
+
+abstract class DoctrineJsonWhereEqualsFilterApplier extends DoctrineFilterApplier
+{
+    public function applyTo($target, FilterAppliersRegistry $appliersRegistry): void
+    {
+        foreach ($this->getSupportedFilters() as $key => $conf) {
+            if ($appliersRegistry->hasFilterWithKey($key)) {
+                /** @var float|int|string $val */
+                $val = $appliersRegistry->getFilterValueForKey($key);
+
+                self::assertValueTypeIsCorrect($key, $val);
+
+                $alias = $this->getModelAlias();
+
+                $target->andWhere("JSON_SEARCH({$alias}.{$conf['column']}, 'one', :{$key}, NULL, '{$conf['path']}') IS NOT NULL")
+                    ->setParameter($key, $val);
+            }
+        }
+    }
+
+    public function supports(FilterAppliersRegistry $appliersRegistry): bool
+    {
+        $supportedFilters = array_flip(array_keys($this->getSupportedFilters()));
+        $requestedFilters = $appliersRegistry->requestedFilters();
+
+        $diff = array_intersect_key($supportedFilters, $requestedFilters);
+
+        return !empty($diff);
+    }
+
+    abstract protected function getModelAlias(): string;
+
+    /**
+     * @return array<string, array{path: string, column: string}>
+     */
+    abstract protected function getSupportedFilters(): array;
+
+    private static function assertValueTypeIsCorrect(string $key, mixed $val): void
+    {
+        if (is_string($val) || is_float($val) || is_int($val)) {
+            return;
+        }
+
+        throw new \InvalidArgumentException(
+            sprintf(
+                'When using the WhereLike applier, the filter value must be an integer, float, or string. [key: %s - val type: %s - val: %s]',
+                $key,
+                gettype($val),
+                is_array($val) ? json_encode($val) : (string) $val,
+            ),
+        );
+    }
+}
---
+++ tests/Integration/DDDStarterPack/Repository/Doctrine/Repository/Filter/DoctrineJsonWhereEqualsFilterApplierTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\DDDStarterPack\Repository\Doctrine\Repository\Filter;
+
+use DDDStarterPack\Repository\Doctrine\Repository\Filter\DoctrineJsonWhereEqualsFilterApplier;
+use DDDStarterPack\Repository\Filter\FilterAppliersRegistryBuilder;
+use DDDStarterPack\Repository\Test\DoctrineUtil;
+use PHPUnit\Framework\TestCase;
+use Tests\Support\Model\Person;
+use Tests\Tool\EntityManagerBuilder;
+
+class DoctrineJsonWhereEqualsFilterApplierTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_supports_multiple_filters(): void
+    {
+        $requestedFilters = ['address' => "O'Connell Street Lower"];
+        $registryBuilder = new FilterAppliersRegistryBuilder();
+        $appliersRegistry = $registryBuilder->build($requestedFilters);
+
+        $applier = new DoctrinePersonJsonWhereEqualsFilterApplier();
+        self::assertTrue($applier->supports($appliersRegistry));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_false_if_not_supports(): void
+    {
+        $requestedFilters = ['height' => 176];
+        $registryBuilder = new FilterAppliersRegistryBuilder();
+        $appliersRegistry = $registryBuilder->build($requestedFilters);
+
+        $applier = new DoctrinePersonJsonWhereEqualsFilterApplier();
+        self::assertFalse($applier->supports($appliersRegistry));
+    }
+
+    /**
+     * @psalm-suppress all
+     *
+     * @test
+     */
+    public function it_should_apply_where_equals_filters(): void
+    {
+        $requestedFilters = ['address' => 'connell', 'vat' => '56789'];
+        $registryBuilder = new FilterAppliersRegistryBuilder();
+        $appliersRegistry = $registryBuilder->build($requestedFilters);
+        $applier = new DoctrinePersonJsonWhereEqualsFilterApplier();
+
+        $em = EntityManagerBuilder::create()->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select('p')->from(Person::class, 'p');
+
+        $applier->applyTo($qb, $appliersRegistry);
+
+        $expected = sprintf(
+            "SELECT p FROM %s p WHERE JSON_SEARCH(p.data, 'one', :address, NULL, '$[*].address') IS NOT NULL AND JSON_SEARCH(p.data, 'one', :vat, NULL, '$[*].general.vat') IS NOT NULL",
+            Person::class,
+        );
+
+        /** @var string $actual */
+        $actual = $qb->getQuery()->getDQL();
+        DoctrineUtil::assertDQLEquals($expected, $actual);
+
+        $addressValue = $qb->getQuery()->getParameters()->getIterator()->offsetGet(0)->getValue();
+        self::assertEquals('connell', $addressValue);
+
+        $vatValue = $qb->getQuery()->getParameters()->getIterator()->offsetGet(1)->getValue();
+        self::assertEquals('56789', $vatValue);
+    }
+
+    /**
+     * @psalm-suppress all
+     *
+     * @test
+     */
+    public function it_should_throw_an_exception_if_the_filter_value_is_not_valid(): void
+    {
+        self::expectException(\InvalidArgumentException::class);
+        self::expectExceptionMessage('When using the WhereEquals applier, the filter value must be an integer, float, or string. [key: address - val type: array - val: ["foo"]]');
+
+        $requestedFilters = ['address' => ['foo']];
+        $registryBuilder = new FilterAppliersRegistryBuilder();
+        $appliersRegistry = $registryBuilder->build($requestedFilters);
+        $applier = new DoctrinePersonJsonWhereEqualsFilterApplier();
+
+        $em = EntityManagerBuilder::create()->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select('p')->from(Person::class, 'p');
+
+        $applier->applyTo($qb, $appliersRegistry);
+    }
+}
+
+class DoctrinePersonJsonWhereEqualsFilterApplier extends DoctrineJsonWhereEqualsFilterApplier
+{
+    protected function getModelAlias(): string
+    {
+        return 'p';
+    }
+
+    protected function getSupportedFilters(): array
+    {
+        return [
+            'address' => ['column' => 'data', 'path' => '$[*].address'],
+            'vat' => ['column' => 'data', 'path' => '$[*].general.vat'],
+        ];
+    }
+}
